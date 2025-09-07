ef9acd9b0da0834538672ffde8a62ee060700884
Yifei Simon Shao <yishao@seas.upenn.edu>
2025-09-07 12:56:08 -0400

    hardware reenable orientation

--- ds_policy/policy.py
+++ ds_policy/policy.py
@@ -296,22 +296,22 @@ class DSPolicy:
                 return np.concatenate([action_pos, action_ang, gripper_action])
             else:
                 p_next, q_next, gamma_pos, gamma_ori, v, w = self.model.step(p_curr, q_curr, self.dt)
-                q_err_simple = self.simple_ds_r_att * q_curr.inv()
-                ori_error_vec = q_err_simple.as_rotvec()
-                action_ang = self.K_ori * ori_error_vec
+                # q_err_simple = self.simple_ds_r_att * q_curr.inv()
+                # ori_error_vec = q_err_simple.as_rotvec()
+                # action_ang = self.K_ori * ori_error_vec
 
 
                 # Use SE3-LPVDS model
                 
                 # apply spherical modulations
-                for obj_center, radius in self.spherical_modulations:
-                    v = spherical_normal_modulation(p_curr, obj_center, radius, v, wrist_pose)
+                # for obj_center, radius in self.spherical_modulations:
+                #     v = spherical_normal_modulation(p_curr, obj_center, radius, v, wrist_pose)
                 
-                # apply ellipsoid modulations
-                for obj_center, axes, rotation_matrix in self.ellipsoid_modulations:
-                    v = ellipsoid_normal_modulation(p_curr, obj_center, axes, v, rotation_matrix, wrist_pose)
+                # # apply ellipsoid modulations
+                # for obj_center, axes, rotation_matrix in self.ellipsoid_modulations:
+                #     v = ellipsoid_normal_modulation(p_curr, obj_center, axes, v, rotation_matrix, wrist_pose)
                 action_pos = v.flatten()
-                # action_ang = w.flatten()
+                action_ang = w.flatten()
                 gripper_action = np.zeros(1)
                 return np.concatenate([action_pos, action_ang, gripper_action])
 
